534e366e42a8275747ac0f2ee2498569dd44c32c
Alex Johnson <ajohnson2389@gmail.com>
2022-03-10 20:52:36 -0500

    minor tweak

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -4,14 +4,14 @@ import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs';
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-      <a href='https://twitter.com/aj_sizzle' target='_blank' rel='noreferrer'>
-        <div>
-          <BsTwitter />
-        </div>
-      </a>
       <a href='https://github.com/ajsizzle' target='_blank' rel='noreferrer'>
         <div>
           <BsGithub />
+        </div>
+      </a>
+      <a href='https://twitter.com/aj_sizzle' target='_blank' rel='noreferrer'>
+        <div>
+          <BsTwitter />
         </div>
       </a>
       <a
